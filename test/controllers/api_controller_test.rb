f31d1209f09a3392f4061f970b24c4a982a009ca
cealvarez <cealvarez@uc.cl>
2016-05-15 03:06:06 -0400

    terminando la api y agregando tests

--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -1,17 +1,24 @@
 require 'test_helper'
-require_relative "../../SimpleNumber"
 class ApiControllerTest < ActionController::TestCase
-  test "should get home" do
-    get :home
+  test "contar posts token malo" do
+  	numero = ApiController.new().contar_post_api('pikachu', '123alguntoken')
+    assert_nil(numero, 'malo')
+  end
+  test "contar posts" do
+  	numero = ApiController.new().contar_post_api('pikachu', '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402')
+    assert_not_nil(numero, 'malo')
+  end
+  test "obtener post token malo" do
+  	get :obtener_post_api, :tag => 'pokemon', :access_token => '2002alguntoken'
+    assert_response :bad_request
+  end
+
+  test "obtener post sin token" do
+  	get :obtener_post_api, :tag => 'pokemon', :access_token => ''
+    assert_response :bad_request
+  end
+  test "obtener post" do
+  	get :obtener_post_api, :tag => 'pokemon', :access_token => '2019746130.59a3f2b.86a0135240404ed5b908a14c0a2d9402',format: :json
     assert_response :success
   end
-  test 'suma' do
-  	assert_equal(4, SimpleNumber.new(2).add(2) )
-  	assert_equal(6, SimpleNumber.new(4).add(2) )
-    assert_equal(9, SimpleNumber.new(3).multiply(3), "Has fallado")
-  end
-  #test "obtener_post_api" do
-  #  post :obtener_post_api
-  #  assert_response :success
-  #end
 end
